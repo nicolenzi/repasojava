c8dfefedd79b01c1eff8947e8956fcdc210e70c6
nico <nicolenzi1@hotmail.com>
2023-08-22 20:43:42 -0300

    nombre

--- src/repaso/Repaso.java
+++ src/repaso/Repaso.java
@@ -14,7 +14,12 @@ public class Repaso {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        int NicoEdad = 38;
+        String NicoNom = "nico";
+        
+        System.out.println("me llamo: "+ NicoNom);
+        System.out.println("y tengo "+ NicoEdad);
+        //nombre
     }
     
 }
